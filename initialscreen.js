d06e53ab1257a5524c3139954c394d478926aa78
SungMin Park <spark259@asu.edu>
2024-07-31 13:15:04 -0700

    new change

---
+++ initialscreen.js
@@ -0,0 +1,9 @@
+document.getElementById('how-to-play-btn').addEventListener('click', function() {
+    alert('How to Play: Here you would explain the rules of the game...');
+    // Add any additional functionality you want here
+});
+
+document.getElementById('play-btn').addEventListener('click', function() {
+    window.location.href = 'index.html'; // Change 'game.html' to the actual game page
+    // Add any additional functionality you want here
+});
