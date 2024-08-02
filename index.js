2ec9f7604a26c824013db29eefd81489c7433280
SungMin Park <spark259@asu.edu>
2024-08-02 08:00:20 -0700

    start button added

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.getElementById('generate-btn').addEventListener('click', function() {
+document.getElementById('start-btn').addEventListener('click', function() {
     const degreeSequenceInput = "4,3,3,2,1";
     const degreeSequence = degreeSequenceInput.split(',').map(Number);
     generateGraph(degreeSequence);
